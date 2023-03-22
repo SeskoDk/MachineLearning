5a44720dc9493f742449b32c53453475a4efc331
Sesko <Sesko.DK@gmail.com>
2023-03-22 16:38:17 +0100

    rename regression and classification dummy functions

--- regression/data.py
+++ regression/data.py
@@ -7,8 +7,8 @@ def affine_function(X: np.ndarray, slope: float = 2.0, intercept: float = 5.0) -
     return slope * X + intercept
 
 
-def dummy_linear_data(N: int = 300, value_range: Tuple = (-10, 10),
-                      mean: float = 0.0, std: float = 5.0) -> Tuple[np.ndarray, np.ndarray]:
+def custom_regression_data(N: int = 300, value_range: Tuple = (-10, 10),
+                           mean: float = 0.0, std: float = 5.0) -> Tuple[np.ndarray, np.ndarray]:
     LOWER_BOUND, UPPER_BOUND = value_range
     X = np.random.uniform(low=LOWER_BOUND, high=UPPER_BOUND, size=N)
     noise = np.random.normal(loc=mean, scale=std, size=N)
@@ -16,8 +16,8 @@ def dummy_linear_data(N: int = 300, value_range: Tuple = (-10, 10),
     return X, y
 
 
-def dummy_classification_data(N: int = 300, n_classes: int = 3,
-                              feat_dim: int = 2, seed: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+def custom_classification_data(N: int = 300, n_classes: int = 3,
+                               feat_dim: int = 2, seed: int = 1) -> Tuple[np.ndarray, np.ndarray]:
     if N % n_classes != 0:
         raise ValueError("Sample size N must be divisible by the number of classes.")
 
@@ -41,7 +41,7 @@ def dummy_classification_data(N: int = 300, n_classes: int = 3,
 
 
 def main():
-    X, y = dummy_classification_data()
+    X, y = custom_classification_data()
     plt.scatter(X[:, 0], X[:, 1], c=y)
     plt.show()
 
